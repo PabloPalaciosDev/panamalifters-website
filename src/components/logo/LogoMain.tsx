9451f3c8af912fdd7fd5884156a11aafa96ce444
Pablo Palacios <72522406+PabloPalaciosDev@users.noreply.github.com>
2025-04-06 01:39:53 -0500

    fix: actualizar dependencias de MUI y React, y corregir tipos de imágenes

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -1,38 +1,15 @@
-// material-ui
-import { useTheme } from '@mui/material/styles';
-
-// project-import
-import { ThemeMode } from 'config';
-
-/**
- * if you want to use image instead of <svg> uncomment following.
- *
- * import logoDark from 'assets/images/logo-dark.svg';
- * import logo from 'assets/images/logo.svg';
- *
- */
-
-// ==============================|| LOGO SVG ||============================== //
+import logo from 'assets/images/logo-panamalifters-notbg.png';
 
 export default function LogoMain({ reverse }: { reverse?: boolean }) {
-  const theme = useTheme();
   return (
-    /**
-     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
-     *
-     * <img src={theme.palette.mode === ThemeMode.DARK ? logoDark : logo} alt="Mantis" width="100" />
-     *
-     */
-    <>
-      <img
-        src="src\assets\images\logo-panamalifters-notbg.png"
-        alt="Mantis"
-        width="200"
-        style={{
-          filter: reverse ? 'invert(1)' : 'none',
-          transition: 'filter 0.3s ease-in-out'
-        }}
-      />
-    </>
+    <img
+      src={logo}
+      alt="Mantis"
+      width="200"
+      style={{
+        filter: reverse ? 'invert(1)' : 'none',
+        transition: 'filter 0.3s ease-in-out'
+      }}
+    />
   );
 }
